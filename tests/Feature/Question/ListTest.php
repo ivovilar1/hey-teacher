0d2c331b25efb1a78897af03dd043af8c7941be5
Ivo <ivo_vilar@hotmail.com>
2023-06-25 10:25:21 -0300

    hey-16: ordering based on likes/unlikes

--- tests/Feature/Question/ListTest.php
+++ tests/Feature/Question/ListTest.php
@@ -25,3 +25,23 @@ it('should paginate the result', function () {
     actingAs($user);
     get(route('dashboard'))->assertViewHas('questions', fn ($value) => $value instanceof LengthAwarePaginator);
 });
+
+it('should order by like and unlike, most liked question should be at the top, most unliked question shoul be at the bottom', function () {
+    $user       = User::factory()->create();
+    $secondUser = User::factory()->create();
+    Question::factory()->count(5)->create();
+    $mostLikedQuestion   = Question::inRandomOrder()->first();
+    $mostUnlikedQuestion = Question::inRandomOrder()->first();
+    $user->like($mostLikedQuestion);
+    $secondUser->unlike($mostUnlikedQuestion);
+    actingAs($user);
+    get(route('dashboard'))
+        ->assertViewHas('questions', function ($questions) use ($mostLikedQuestion, $mostUnlikedQuestion) {
+            expect($questions)
+                ->first()->id->toBe($mostLikedQuestion->id)
+                ->and($questions)
+                ->last()->id->toBe($mostUnlikedQuestion->id);
+
+            return true;
+        });
+});
